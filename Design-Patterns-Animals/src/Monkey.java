9b753797e6e0cb9f67c02a976f5a6b622b366b9a
Jerry C <54548815+chiujienwu@users.noreply.github.com>
2019-11-17 23:07:31 -0600

    initial
    DESIGN PATTERNS VIDEO TUTORIAL 2 - Derek Banas
    
    http://www.newthinktank.com/2012/08/design-patterns-video-tutorial-2/

---
+++ Design-Patterns-Animals/src/Monkey.java
@@ -0,0 +1,75 @@
+public class Monkey implements Living{
+
+    private String name;
+    private double weight;
+    private double height;
+    private String favFood;
+    private double speed;
+    private String sound;
+
+    @Override
+    public void setName(String newName) {
+        name = newName;
+
+    }
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return name;
+    }
+
+    @Override
+    public void setHeight(double newheight) {
+        height = newheight;
+
+    }
+
+    @Override
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public void setWeight(double newWeight) {
+        weight = newWeight;
+
+    }
+
+    @Override
+    public double getWeight() {
+        // TODO Auto-generated method stub
+        return weight;
+        // return 0;
+    }
+
+    @Override
+    public void setFavFood(String newFood) {
+        favFood = newFood;
+    }
+
+    @Override
+    public String getFavFood() {
+        return favFood;
+    }
+
+    @Override
+    public void setSpeed(double newSpeed) {
+        speed = newSpeed;
+    }
+
+    @Override
+    public double getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public void setSound(String newSound) {
+        sound = newSound;
+    }
+
+    @Override
+    public String getSound() {
+        return sound;
+    }
+}
